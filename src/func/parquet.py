e27c698dc7c9b9f920681f9327b38b60b10d3c4d
Dim Kharitonov <dimds@fastmail.com>
2023-12-24 11:13:27 +0100

    docs: docstring added

--- src/func/parquet.py
+++ src/func/parquet.py
@@ -18,6 +18,15 @@ def create_dataset_from_filesystem(
 ) -> DatasetOptional:
     """
     Creates PyArrow dataset from local folder.
+
+    Args:
+        path: Path to the folder containing the parquet files.
+        schema: Schema of the parquet files.
+        filesystem: PyArrow filesystem object.
+        dataset_factory: Function to create the PyArrow dataset.
+
+    Returns:
+        PyArrow dataset object or None if the dataset creation fails
     """
     try:
         dataset = dataset_factory(source=path, schema=schema, filesystem=filesystem)
