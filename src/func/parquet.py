5c079830cf5196e9f973606e98927b14144cf6ad
Dim Kharitonov <dimds@fastmail.com>
2023-12-24 22:34:18 +0100

    iterators for data access

--- src/func/parquet.py
+++ src/func/parquet.py
@@ -1,6 +1,8 @@
 """Functions to read parquet files"""
 
-from typing import Callable, Union
+import logging
+from itertools import islice
+from typing import Any, Callable, Generator, Union
 
 import pyarrow as pa
 import pyarrow.dataset as ds
@@ -8,11 +10,14 @@ from pyarrow import fs
 
 DatasetOptional = Union[ds.Dataset, None]
 DataSetFactoryFunction = Callable[[str, pa.schema, fs.FileSystem], DatasetOptional]
-
+DataSetProcessFunction = Callable[[dict, Any], Any]
+DictOptional = Union[dict[str, list[Any]], None]
+ListOptional = Union[list[dict[str, Any]], None]
 
 def create_dataset_from_filesystem(
+    logger: logging.Logger,
     path: str,
-    schema: pa.schema,
+    schema: pa.Schema,
     filesystem: fs.FileSystem,
     dataset_factory: DataSetFactoryFunction = ds.dataset,
 ) -> DatasetOptional:
@@ -20,6 +25,7 @@ def create_dataset_from_filesystem(
     Creates PyArrow dataset from local folder.
 
     Args:
+        logger: Logger object.
         path: Path to the folder containing the parquet files.
         schema: Schema of the parquet files.
         filesystem: PyArrow filesystem object.
@@ -29,8 +35,90 @@ def create_dataset_from_filesystem(
         PyArrow dataset object or None if the dataset creation fails
     """
     try:
+        logger.debug(f"Creating dataset from {path}, schema: {schema}")
         dataset = dataset_factory(source=path, schema=schema, filesystem=filesystem)
     except Exception as e:
+        logger.error(f"Failed to create dataset from {path}: {e}")
         dataset = None
 
     return dataset
+
+
+# returns iterator of RecordBatch
+def get_record_batch_iterator(
+    dataset: DatasetOptional, schema: pa.Schema, batch_size: int
+) -> Generator[pa.RecordBatch, None, None]:
+    """
+    Returns iterator over dataset batches.
+
+    Args:
+        dataset: PyArrow dataset object.
+        schema: Schema of the parquet files.
+        batch_size: Size of the batches.
+
+    Returns:
+        Iterator over dataset batches.
+    """
+
+    # if dataset is None return empty iterator
+    # else return iterator over dataset batches
+    return (
+        iter(
+            dataset.to_batches(
+                columns=schema.names, batch_size=batch_size, use_threads=True
+            )
+        )
+        if dataset
+        else iter([])
+    )
+
+
+def get_mongo_update_iterator(
+    record_batch_iterator: Generator[pa.RecordBatch, None, None], mongo_batch_size: int
+) -> Generator[pa.RecordBatch, None, None]:
+    """
+    Returns iterator over dataset batches.
+
+    Args:
+        record_batch_iterator: PyArrow RecordBatch Iterator.
+        mongo_batch_size: Size of the batches.
+
+    Returns:
+        Iterator over dataset batches.
+    """
+
+    # if dataset is None return empty iterator
+    # else return iterator over dataset batches
+    return (
+        iter(islice(record_batch_iterator, 0, None, mongo_batch_size))
+        if record_batch_iterator
+        else iter([])
+    )
+
+def transform_dict_to_list(dict_data: DictOptional) -> ListOptional:
+    """
+    Transform a dictionary with table column names as keys and rows as values
+    to a list of dictionaries where each dictionary represents a row.
+
+    Args:
+        dict_data (dict): Dictionary with table column names as keys and rows as values.
+
+    Returns:
+        list: List of dictionaries, where each dictionary represents a row.
+    """
+    if not dict_data:
+        return None
+
+    # Get column names from the first row
+    columns = list(dict_data.keys())
+
+    # Determine the number of rows (assuming all rows have the same length)
+    num_rows = len(next(iter(dict_data.values())))
+
+    # Create a list of dictionaries for each row
+    result = [
+        {column: dict_data[column][row_index] for column in columns}
+        for row_index in range(num_rows)
+    ]
+
+    return result
--- tests/test_parquet.py
+++ tests/test_parquet.py
@@ -3,7 +3,13 @@ from unittest.mock import Mock
 import pyarrow as pa
 import pyarrow.dataset as ds
 
-from src.func.parquet import create_dataset_from_filesystem
+from src.func.parquet import (
+    create_dataset_from_filesystem,
+    get_mongo_update_iterator,
+    get_record_batch_iterator,
+    transform_dict_to_list,
+)
+
 
 def test_create_dataset_from_filesystem():
     # Mocking the necessary objects
@@ -12,16 +18,20 @@ def test_create_dataset_from_filesystem():
     empty_table = pa.table([[], []], schema=schema)
     dummy_dataset = ds.dataset([empty_table])
     filesystem = Mock()
+    logger = Mock()
 
     # Mocking the dataset factory function to return a dummy dataset
     dataset_factory_mock = Mock()
     dataset_factory_mock.return_value = dummy_dataset
 
     # Mocking another dataset factory that raises an exception
-    dataset_factory_exception_mock = Mock(side_effect=Exception("Dataset creation failed"))
+    dataset_factory_exception_mock = Mock(
+        side_effect=Exception("Dataset creation failed")
+    )
 
     # Call the function with the mocked objects
     dataset = create_dataset_from_filesystem(
+        logger=logger,
         path=path,
         schema=schema,
         filesystem=filesystem,
@@ -29,6 +39,7 @@ def test_create_dataset_from_filesystem():
     )
 
     dataset_exception = create_dataset_from_filesystem(
+        logger=logger,
         path=path,
         schema=schema,
         filesystem=filesystem,
@@ -46,3 +57,94 @@ def test_create_dataset_from_filesystem():
 
     # Verify that the dataset returned by the function is None
     assert dataset_exception is None
+
+
+def test_dataset_batches():
+    # Mocking the necessary objects
+    batch_size = 2
+
+    # Create a dummy PyArrow schema for testing
+    schema = pa.schema([("column1", pa.int64()), ("column2", pa.string())])
+
+    # Create a dummy PyArrow table for testing
+    table = pa.table(
+        {"column1": [1, 2, 3, 4, 5], "column2": ["a", "b", "c", "d", "e"]},
+        schema=schema,
+    )
+
+    # Create a dummy PyArrow dataset with the table
+    dataset = ds.dataset(table)
+
+    # Call the function with the mocked objects
+    batches = list(get_record_batch_iterator(dataset, schema, batch_size))
+
+    # Verify that the correct batches are returned
+    assert len(batches) == 3  # Dataset size is 5, batch size is 2 -> 3 batches
+    assert isinstance(batches[0], pa.RecordBatch)
+    assert batches[0].num_rows == 2  # Batch size is 2
+
+    # Test with None dataset
+    empty_batches = list(get_record_batch_iterator(None, schema, batch_size))
+    assert len(empty_batches) == 0  # No batches should be returned for None dataset
+
+
+def test_get_mongo_update_iterator():
+    # Mocking the necessary objects
+    batch_size = 2
+
+    # Create a dummy PyArrow schema for testing
+    schema = pa.schema([("column1", pa.int64()), ("column2", pa.string())])
+
+    # Create a dummy PyArrow table for testing
+    table = pa.table(
+        {"column1": [1, 2, 3, 4, 5], "column2": ["a", "b", "c", "d", "e"]},
+        schema=schema,
+    )
+
+    # Create a dummy PyArrow dataset with the table
+    dataset = ds.dataset(table)
+
+    # Call the function with the mocked objects
+    record_iterator = get_record_batch_iterator(dataset, schema, batch_size)
+
+    # Call the function with the mocked objects
+    mongo_update_iterator = get_mongo_update_iterator(record_iterator, batch_size)
+
+    # Verify that the correct batches are returned
+    batches = list(mongo_update_iterator)
+    assert len(batches) == 2  # Record batch size is 3, batch size is 2 -> 2 batches
+    assert isinstance(batches[0], pa.RecordBatch)
+    assert batches[0].num_rows == 2  # Assuming the dataset size is 2
+
+    # Test with None record_batch_iterator
+    empty_iterator = get_mongo_update_iterator(None, batch_size)
+    assert (
+        list(empty_iterator) == []
+    )  # No batches should be returned for None record_batch_iterator
+
+def test_transform_dict_to_list():
+    # Test with a dictionary containing columns 'ID', 'Name', and 'Age'
+    input_dict = {
+        'ID': [1, 2, 3],
+        'Fruit': ['Apple', 'Orange', 'Plum'],
+        'Color': ['Green', 'Orange', 'Purple']
+    }
+
+    expected_result = [
+        {'ID': 1, 'Fruit': 'Apple', 'Color': 'Green'},
+        {'ID': 2, 'Fruit': 'Orange', 'Color': 'Orange'},
+        {'ID': 3, 'Fruit': 'Plum', 'Color': 'Purple'}
+    ]
+
+    result_list = transform_dict_to_list(input_dict)
+
+    assert result_list == expected_result
+
+    # Test with an empty dictionary
+    empty_dict = {}
+    assert transform_dict_to_list(empty_dict) == None
+
+    # Test with a dictionary with one column
+    single_column_dict = {'ID': [1, 2, 3]}
+    expected_single_column_result = [{'ID': 1}, {'ID': 2}, {'ID': 3}]
+    assert transform_dict_to_list(single_column_dict) == expected_single_column_result
