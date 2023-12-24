f7fb17a63e4a88f8d81f472ca64d63c11ecbc07a
Dim Kharitonov <dimds@fastmail.com>
2023-12-24 11:11:47 +0100

    introduced optional result

--- src/func/parquet.py
+++ src/func/parquet.py
@@ -1,18 +1,27 @@
 """Functions to read parquet files"""
 
+from typing import Callable, Union
+
 import pyarrow as pa
 import pyarrow.dataset as ds
 from pyarrow import fs
+
+DatasetOptional = Union[ds.Dataset, None]
+DataSetFactoryFunction = Callable[[str, pa.schema, fs.FileSystem], DatasetOptional]
 
 
 def create_dataset_from_filesystem(
     path: str,
     schema: pa.schema,
     filesystem: fs.FileSystem,
-    dataset_factory: callable = ds.dataset,
-):
+    dataset_factory: DataSetFactoryFunction = ds.dataset,
+) -> DatasetOptional:
     """
     Creates PyArrow dataset from local folder.
     """
-    dataset = dataset_factory(source=path, schema=schema, filesystem=filesystem)
+    try:
+        dataset = dataset_factory(source=path, schema=schema, filesystem=filesystem)
+    except Exception as e:
+        dataset = None
+
     return dataset
--- tests/test_parquet.py
+++ tests/test_parquet.py
@@ -1,19 +1,24 @@
 from unittest.mock import Mock
 
 import pyarrow as pa
-from pyarrow import fs
+import pyarrow.dataset as ds
 
 from src.func.parquet import create_dataset_from_filesystem
-
 
 def test_create_dataset_from_filesystem():
     # Mocking the necessary objects
     path = "path/to/data"
-    dummy_dataset = "dataset"
     schema = pa.schema([("column1", pa.int64()), ("column2", pa.string())])
+    empty_table = pa.table([[], []], schema=schema)
+    dummy_dataset = ds.dataset([empty_table])
     filesystem = Mock()
+
+    # Mocking the dataset factory function to return a dummy dataset
     dataset_factory_mock = Mock()
     dataset_factory_mock.return_value = dummy_dataset
+
+    # Mocking another dataset factory that raises an exception
+    dataset_factory_exception_mock = Mock(side_effect=Exception("Dataset creation failed"))
 
     # Call the function with the mocked objects
     dataset = create_dataset_from_filesystem(
@@ -21,6 +26,13 @@ def test_create_dataset_from_filesystem():
         schema=schema,
         filesystem=filesystem,
         dataset_factory=dataset_factory_mock,
+    )
+
+    dataset_exception = create_dataset_from_filesystem(
+        path=path,
+        schema=schema,
+        filesystem=filesystem,
+        dataset_factory=dataset_factory_exception_mock,
     )
 
     # Verify that the dataset factory function was called with the correct arguments
@@ -31,3 +43,6 @@ def test_create_dataset_from_filesystem():
     # Verify that the dataset returned by the function is the same as
     # the one returned by the dataset factory function
     assert dataset == dummy_dataset
+
+    # Verify that the dataset returned by the function is None
+    assert dataset_exception is None
